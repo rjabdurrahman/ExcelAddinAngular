cd9d38845353c369b45472ea75152a30cac57903
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-09-01 17:47:37 +0600

    Integrated: App Dashboard

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import AppComponent from "./app.component";
 import LoginComponent from './login/login.component';
+import DashboardComponent from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
